9dc6468d059e1ccbd670ac2402f34f1a6d9e4fe1
thomson08 <114720277+thomson08@users.noreply.github.com>
2024-04-30 22:27:57 +0000

    Testing MySQL connection

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT first_name from user;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.first_name}")
